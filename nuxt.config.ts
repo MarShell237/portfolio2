bd62bfc8ec9db3240379447d892675c52a9c68b7
MarShell237 <marceldjiofack@outlook.com>
2024-11-02 12:01:37 +0100

    ajout des balises meta og

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,16 @@ export default defineNuxtConfig({
       charset:'utf-8',
       viewport:'width=device-width, initial-scale=1',
       title: 'Marcel J. DJIOFACK portfolio',
+      htmlAttrs: { lang: 'fr' },
       meta: [
-        { name: 'description', content: 'Portfolio de Marcel J. DJIOFACK' }
+        { name: 'description', content: 'Portfolio de Marcel J. DJIOFACK' },
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { property: 'og:title', content: 'Mon site' },
+        { property: 'og:description', content: 'Portfolio de Marcel J. DJIOFACK' },
+        { property: 'og:image', content: '/assets/images/logo.png' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://portfolio-nine-jet-89.vercel.app/' },
       ],
       script: [
         { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js' }
